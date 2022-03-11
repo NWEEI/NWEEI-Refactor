0b51fba3e8b64207b4901456c9ccf30c97930dec
Joshua Allison <63320691+joshua-allison@users.noreply.github.com>
2022-03-10 21:30:01 -0800

    Added Annotations
    Added annotations to domain models

--- NWEEI-V2/Models/Domain/FAQ.cs
+++ NWEEI-V2/Models/Domain/FAQ.cs
@@ -1,9 +1,13 @@
-﻿namespace NWEEI_V2.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NWEEI_V2.Models
 {
     public class FAQ : ContentItem
     {
         public int FAQID { get; set; }
+        [Required(ErrorMessage = "A(n) {0} is required.")]
         public string Question { get; set; }
+        [Required(ErrorMessage = "A(n) {0} is required.")]
         public string Answer { get; set; }
         public int Upvotes { get; set; }
     }
