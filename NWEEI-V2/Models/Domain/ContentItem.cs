a4089ab5c27f0ff2b481cd11a9958bf3d01e1cee
Joshua Allison <63320691+joshua-allison@users.noreply.github.com>
2022-03-10 19:28:03 -0800

    Added Domain Models

---
+++ NWEEI-V2/Models/Domain/ContentItem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NWEEI_V2.Models.Domain
+{
+    public class ContentItem
+    {
+    }
+}
