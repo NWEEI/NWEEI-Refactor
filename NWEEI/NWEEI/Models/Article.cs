5af53515608a5f6e617070db2f7f20f2bb3ec01d
jeccaarthur <63317970+jeccaarthur@users.noreply.github.com>
2022-04-04 14:57:08 -0700

    Added properties

--- NWEEI/NWEEI/Models/Article.cs
+++ NWEEI/NWEEI/Models/Article.cs
@@ -8,6 +8,10 @@ namespace NWEEI.Models
         public string Title { get; set; }
         public string Body { get; set; }
         public DateTime DateCreated { get; set; }
+        public AppUser Author { get; set; }
+        public Category Category { get; set; }
+        public bool Published { get; set; }
+        public bool Featured { get; set; }
         public int Views { get; set; }
     }
 }
