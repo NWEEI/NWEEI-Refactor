7395a24bc065b07cea9c346a242e441c0ead10ab
Joshua Allison <63320691+joshua-allison@users.noreply.github.com>
2022-04-18 15:30:06 -0700

    Added Identity Scaffolded Items

---
+++ NWEEI/NWEEI/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NWEEI.Data;
+using NWEEI.Models;
+
+[assembly: HostingStartup(typeof(NWEEI.Areas.Identity.IdentityHostingStartup))]
+namespace NWEEI.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
