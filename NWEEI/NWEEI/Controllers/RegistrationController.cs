17b235952715a844f96be7cff4a1f51217497c0a
Joshua Allison <63320691+joshua-allison@users.noreply.github.com>
2022-04-23 17:40:51 -0700

    Updated Registration Interactions
    - Restricted all registration controller methods to the Admin role, excluding create.
    - Removed most fields under the Registration>Delete view, leaving just a summary.
    - Modified the layout of edit to be a bit more compact, and for each field to be near related fields, and to take up an appropriate amount of space.
    - Modified the "Register" link in top-level navigation to link to Registration>Create, rather than Registration>Index.

--- NWEEI/NWEEI/Controllers/RegistrationController.cs
+++ NWEEI/NWEEI/Controllers/RegistrationController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -20,12 +21,14 @@ namespace NWEEI.Controllers
         }
 
         // GET: Registration
+        [Authorize(Roles ="Admin")]
         public async Task<IActionResult> Index()
         {
             return View(await _context.Registrations.OrderByDescending(r => r.DateSubmitted).ToListAsync());
         }
 
         // GET: Registration/Details/5
+        [Authorize(Roles = "Admin")]
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -66,6 +69,7 @@ namespace NWEEI.Controllers
         }
 
         // GET: Registration/Edit/5
+        [Authorize(Roles = "Admin")]
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -86,6 +90,7 @@ namespace NWEEI.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin")]
         public async Task<IActionResult> Edit(int id, [Bind("RegistrationID,TrainingProgram,FirstName,LastName,Email,DateOfBirth,Title,Organization,Address1,Address2,City,State,ZipCode,Country,Phone,Fax,Referral,SpecialInstructions,PaymentType")] Registration registration)
         {
             if (id != registration.RegistrationID)
@@ -117,6 +122,7 @@ namespace NWEEI.Controllers
         }
 
         // GET: Registration/Delete/5
+        [Authorize(Roles = "Admin")]
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -135,6 +141,7 @@ namespace NWEEI.Controllers
         }
 
         // POST: Registration/Delete/5
+        [Authorize(Roles = "Admin")]
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
