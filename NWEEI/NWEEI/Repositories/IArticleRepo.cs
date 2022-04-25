3b0c870c2df1b9c49f3b4a0a891b8e8a9d3d8370
jeccaarthur <63317970+jeccaarthur@users.noreply.github.com>
2022-04-24 22:43:24 -0700

    Added article retrieval methods

---
+++ NWEEI/NWEEI/Repositories/IArticleRepo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NWEEI.Models;
+
+namespace NWEEI.Repositories
+{
+    public interface IArticleRepo
+    {
+        // retrieval methods
+        IQueryable<Article> Articles { get; }
+        List<Article> GetAllArticles();
+        List<Article> GetArticlesByCategoryID(int categoryID);
+        List<Article> GetArticlesBySearchQuery(string query);
+        Article GetArticleByID(int id);
+
+    }
+}
