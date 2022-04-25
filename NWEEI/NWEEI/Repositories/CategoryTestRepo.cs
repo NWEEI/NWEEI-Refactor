b9526cfab2ab3f6e32e00aa7f39687269dc033a5
jeccaarthur <63317970+jeccaarthur@users.noreply.github.com>
2022-04-25 12:36:32 -0700

    Added Category interface, repo, and test repo with basic retrieval methods

---
+++ NWEEI/NWEEI/Repositories/CategoryTestRepo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using NWEEI.Models;
+
+namespace NWEEI.Repositories
+{
+    public class CategoryTestRepo : ICategoryRepo
+    {
+        private List<Category> categories = new List<Category>();
+
+        public IQueryable<Category> Categories
+        {
+            get
+            {
+                return categories.AsQueryable<Category>();
+            }
+        }
+
+        public List<Category> GetAllCategories()
+        {
+            categories = Categories.ToList();
+
+            return categories;
+        }
+
+        public Category GetCategoryByID(int id)
+        {
+            Category category = categories.Find(c => c.CategoryID == id);
+            return category;
+        }
+    }
+}
---
+++ NWEEI/NWEEI/Repositories/ICategoryRepo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NWEEI.Models;
+
+namespace NWEEI.Repositories
+{
+    public interface ICategoryRepo
+    {
+        // retrieval methods
+        IQueryable<Category> Categories { get; }
+        List<Category> GetAllCategories();
+        Category GetCategoryByID(int id);
+    }
+}
