6e5513098523f7fe087a7ab313ff7c5c8db7efbe
Joshua Allison <63320691+joshua-allison@users.noreply.github.com>
2022-05-07 20:19:55 -0700

    Admin Dashboard
    - Removed links on login partial for admin access
    - Replaced the above with a link to "Dashboard"
    - Added a Dashboard view under the home controller (might need to be moved later on).
    - Added custom css for new html class: "dashboard-tile"
    - Removed Privacy view, not in use.

--- NWEEI/NWEEI/Controllers/HomeController.cs
+++ NWEEI/NWEEI/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using NWEEI.Models;
@@ -24,7 +25,8 @@ namespace NWEEI.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        [Authorize(Roles = "Admin")]
+        public IActionResult Dashboard()
         {
             return View();
         }
