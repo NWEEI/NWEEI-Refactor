5e39abb2be87706155007bbe092485974f1fd378
Joshua Allison <63320691+joshua-allison@users.noreply.github.com>
2022-05-08 21:17:51 -0700

    Updated Article Buttons with Tag Helpers
    If there is a button within the article views, then it has been decorated using the custom tag helper "data-btn-[action]"
    Also moved some stuff around, and removed unnecessary stuff.

--- NWEEI/NWEEI/TagHelpers/Buttons/CreateButtonTagHelper.cs
+++ NWEEI/NWEEI/TagHelpers/Buttons/CreateButtonTagHelper.cs
@@ -1,11 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
 
-namespace NWEEI.TagHelpers.Buttons
+namespace NWEEI.TagHelpers
 {
-    public class CreateButtonTagHelper
+    [HtmlTargetElement("a", Attributes = "data-btn-create")]
+    public class CreateButtonATagHelper : TagHelper
     {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.SetRawPreContentElement("<i class=\"fa fa-circle-plus fa-xl\"></i>");
+            output.BuildTag("a", "btn btn-outline-success h3 m-0 mr-2");
+            output.Attributes.SetAttribute("style", "border-color: rgba(0,0,0,0)");
+        }
+    }
+    [HtmlTargetElement("button", Attributes = "data-btn-create")]
+    public class CreateButtonTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.SetRawPreContentElement("<i class=\"fa fa-circle-plus fa-xl\"></i>");
+            output.BuildTag("button", "btn btn-outline-success h3 m-0 mr-2");
+            output.Attributes.SetAttribute("style", "border-color: rgba(0,0,0,0)");
+        }
     }
 }
