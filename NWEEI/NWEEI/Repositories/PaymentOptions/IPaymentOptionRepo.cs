2a95c899c5e717da2eb7dea11947bdb11a7becea
Joshua Allison <63320691+joshua-allison@users.noreply.github.com>
2022-05-09 17:56:46 -0700

    Added PaymentOption Repos

---
+++ NWEEI/NWEEI/Repositories/PaymentOptions/IPaymentOptionRepo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NWEEI.Models;
+
+namespace NWEEI.Repositories
+{
+    public interface ITagRepo
+    {
+        IQueryable<Tag> Tags { get; }
+
+        // create
+        void AddTag(Tag tag);
+
+        // retrieve
+        List<Tag> GetAllTags();
+        Tag GetTagByID(int id);
+
+        // update
+        void UpdateTag(Tag tag);
+
+        // delete
+        void DeleteTag(Tag tag);
+    }
+}
---
+++ NWEEI/NWEEI/Repositories/PaymentOptions/PaymentOptionRepo.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using NWEEI.Data;
+using NWEEI.Models;
+
+namespace NWEEI.Repositories
+{
+    public class PaymentOptionRepo : IPaymentOptionRepo
+    {
+        private NWEEIContext context;
+
+        public PaymentOptionRepo(NWEEIContext c)
+        {
+            context = c;
+        }
+
+        public IQueryable<Tag> Tags
+        {
+            get
+            {
+                return context.Tags;
+            }
+        }
+
+        // add a new tag
+        public void AddTag(Tag tag)
+        {
+            context.Tags.Add(tag);
+            context.SaveChanges();
+        }
+
+        #region retrieval methods
+
+        // get a list of all tags
+        public List<Tag> GetAllTags()
+        {
+            List<Tag> tags = context.Tags.ToList();
+
+            return tags;
+        }
+
+        // get a specific tag by its id
+        public Tag GetTagByID(int id)
+        {
+            Tag tag = context.Tags
+                .Where(t => t.TagID == id)
+                .FirstOrDefault();
+
+            return tag;
+        }
+
+        #endregion
+
+        // update a tag
+        public void UpdateTag(Tag tag)
+        {
+            context.Tags.Update(tag);
+            context.SaveChanges();
+        }
+
+        // delete a tag
+        public void DeleteTag(Tag tag)
+        {
+            Tag existingTag = context.Tags.Find(tag.TagID);
+            context.Tags.Remove(existingTag);
+            context.SaveChanges();
+        }
+    }
+}
