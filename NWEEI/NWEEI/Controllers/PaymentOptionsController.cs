6ed6232e7e7ee9599ebc4e04e0c265546138188e
Joshua Allison <63320691+joshua-allison@users.noreply.github.com>
2022-05-09 17:34:31 -0700

    Added PaymentOptions Model
    With scaffolded controllers and views.

---
+++ NWEEI/NWEEI/Controllers/PaymentOptionsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using NWEEI.Data;
+using NWEEI.Models;
+
+namespace NWEEI.Controllers
+{
+    public class PaymentOptionsController : Controller
+    {
+        private readonly NWEEIContext _context;
+
+        public PaymentOptionsController(NWEEIContext context)
+        {
+            _context = context;
+        }
+
+        // GET: PaymentOptions
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.PaymentOptions.ToListAsync());
+        }
+
+        // GET: PaymentOptions/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var paymentOption = await _context.PaymentOptions
+                .FirstOrDefaultAsync(m => m.PaymentOptionID == id);
+            if (paymentOption == null)
+            {
+                return NotFound();
+            }
+
+            return View(paymentOption);
+        }
+
+        // GET: PaymentOptions/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PaymentOptions/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("PaymentOptionID,Option")] PaymentOption paymentOption)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(paymentOption);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(paymentOption);
+        }
+
+        // GET: PaymentOptions/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var paymentOption = await _context.PaymentOptions.FindAsync(id);
+            if (paymentOption == null)
+            {
+                return NotFound();
+            }
+            return View(paymentOption);
+        }
+
+        // POST: PaymentOptions/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("PaymentOptionID,Option")] PaymentOption paymentOption)
+        {
+            if (id != paymentOption.PaymentOptionID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(paymentOption);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PaymentOptionExists(paymentOption.PaymentOptionID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(paymentOption);
+        }
+
+        // GET: PaymentOptions/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var paymentOption = await _context.PaymentOptions
+                .FirstOrDefaultAsync(m => m.PaymentOptionID == id);
+            if (paymentOption == null)
+            {
+                return NotFound();
+            }
+
+            return View(paymentOption);
+        }
+
+        // POST: PaymentOptions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var paymentOption = await _context.PaymentOptions.FindAsync(id);
+            _context.PaymentOptions.Remove(paymentOption);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PaymentOptionExists(int id)
+        {
+            return _context.PaymentOptions.Any(e => e.PaymentOptionID == id);
+        }
+    }
+}
---
+++ NWEEI/NWEEI/Models/PaymentOption.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NWEEI.Models
+{
+    public class PaymentOption
+    {
+        public int PaymentOptionID { get; set; }
+        [Required(ErrorMessage = "Please enter a payment option.")]
+        public string Option { get; set; }
+    }
+}
