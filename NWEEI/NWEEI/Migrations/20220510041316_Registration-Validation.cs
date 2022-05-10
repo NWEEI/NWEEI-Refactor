ed09447090d0acf872e18cd151faa7a693834de3
Joshua Allison <63320691+joshua-allison@users.noreply.github.com>
2022-05-09 21:16:06 -0700

    Added Registration DOB validation

---
+++ NWEEI/NWEEI/Migrations/20220510041316_Registration-Validation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NWEEI.Migrations
+{
+    public partial class RegistrationValidation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateOfBirth",
+                table: "Registrations",
+                type: "datetime",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "DateOfBirth",
+                table: "Registrations",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime");
+        }
+    }
+}
