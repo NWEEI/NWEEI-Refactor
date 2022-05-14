9d9861c9bce43d002b19198417f017f5999531a1
jeccaarthur <63317970+jeccaarthur@users.noreply.github.com>
2022-05-14 12:55:02 -0700

    Added property for indexing current category in category selector

--- NWEEI/NWEEI/ViewModels/CategorySelectorViewModel.cs
+++ NWEEI/NWEEI/ViewModels/CategorySelectorViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using NWEEI.Models;
 
 namespace NWEEI.ViewModels
@@ -8,6 +9,7 @@ namespace NWEEI.ViewModels
     {
         public List<Category> Categories { get; set; }
         public Category CurrentCategory { get; set; }
+        public int IndexOfCurrentCategory { get; set; }
         public int NewCategoryID { get; set; }
 
         public FAQ CurrentFAQ { get; set; }
