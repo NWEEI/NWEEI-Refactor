fea4d4e5b2f40b4a07e2f5eb20fd53fb07f75d11
jeccaarthur <63317970+jeccaarthur@users.noreply.github.com>
2022-05-13 21:08:16 -0700

    New view model for category selection when creating or editing articles and FAQs

---
+++ NWEEI/NWEEI/ViewModels/CategorySelectorViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using NWEEI.Models;
+
+namespace NWEEI.ViewModels
+{
+    public class CategorySelectorViewModel
+    {
+        public List<Category> Categories { get; set; }
+        public Category CurrentCategory { get; set; }
+        public int NewCategoryID { get; set; }
+
+        public FAQ CurrentFAQ { get; set; }
+        public Article CurrentArticle { get; set; }
+    }
+}
