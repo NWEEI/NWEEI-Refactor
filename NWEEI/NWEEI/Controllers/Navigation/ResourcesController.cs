bdd086ef379a1d314c5dd385349abd35465a4761
jeccaarthur <63317970+jeccaarthur@users.noreply.github.com>
2022-05-15 23:10:49 -0700

    Added view model and controller methods for search functionality. Search section of view is partially styled

--- NWEEI/NWEEI/Controllers/Navigation/ResourcesController.cs
+++ NWEEI/NWEEI/Controllers/Navigation/ResourcesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using NWEEI.Repositories;
+using NWEEI.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -18,12 +19,48 @@ namespace NWEEI.Controllers.Navigation
             repo = r;
         }
 
+        // GET: /Resources/Search
+        // search articles, FAQs, organizations
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.Current = "Resources";
+
+            // initialize new searchVM
+            SearchVM viewModel = new SearchVM();
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        // search articles, FAQs, organizations
+        public async Task<IActionResult> Index(SearchVM viewModel)
+        {
+            ViewBag.Current = "Resources";
+
+            if (ModelState.IsValid)
+            {
+                // get lists of articles, FAQs, and organizations
+                // that contain the search query
+                viewModel.Articles = repo.GetArticlesBySearchQuery(viewModel.SearchQuery);
+                viewModel.FAQs = repo.GetFAQsBySearchQuery(viewModel.SearchQuery);
+                viewModel.Organizations = repo.GetOrgsBySearchQuery(viewModel.SearchQuery);
+
+                return View(viewModel);
+            }
+
+            return NotFound();
+        }
+
+        /*
         // GET: /<controller>/
         public IActionResult Index()
         {
             ViewBag.Current = "Resources";
             return View();
         }
+        */
+
         public IActionResult CareerMap()
         {
             ViewBag.Current = "Resources";
---
+++ NWEEI/NWEEI/ViewModels/SearchVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using NWEEI.Models;
+
+namespace NWEEI.ViewModels
+{
+    public class SearchVM
+    {
+        public string SearchQuery { get; set; }
+        public List<Article> Articles { get; set; }
+        public List<FAQ> FAQs { get; set; }
+        public List<Organization> Organizations { get; set; }
+    }
+}
