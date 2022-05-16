37cfd2a6c88f21d5e6ad5796521034a1706d5674
jeccaarthur <63317970+jeccaarthur@users.noreply.github.com>
2022-05-15 21:34:44 -0700

    Created repositories for resources and added methods for retrieval by search query, and injected into controller

--- NWEEI/NWEEI/Controllers/Navigation/ResourcesController.cs
+++ NWEEI/NWEEI/Controllers/Navigation/ResourcesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using NWEEI.Repositories;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,6 +11,13 @@ namespace NWEEI.Controllers.Navigation
 {
     public class ResourcesController : Controller
     {
+        IResourceRepo repo;
+
+        public ResourcesController(IResourceRepo r)
+        {
+            repo = r;
+        }
+
         // GET: /<controller>/
         public IActionResult Index()
         {
