b8ab306425d8fa1dfa14c453aa5663558e3b0b03
Joshua Allison <63320691+joshua-allison@users.noreply.github.com>
2022-06-04 12:48:11 -0700

    Encapsulated Form Button Tag Helpers
    Now it will be very easy to implement a form button tag helper, which makes it very easy to implement form buttons. See "FormSendButtonTagHelper" to see how I easily implemented the tag-helper, and look at the ContactUs view form submit button to see how easy it was to implement the submit button in cshtml.

--- NWEEI/NWEEI/TagHelpers/Buttons/FormSendButtonTagHelper.cs
+++ NWEEI/NWEEI/TagHelpers/Buttons/FormSendButtonTagHelper.cs
@@ -1,31 +1,43 @@
 ﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using MimeKit.Text;
+using NWEEI.TagHelpers;
 
 namespace NWEEI.TagHelpers
 {
     [HtmlTargetElement("button", Attributes = "data-btn-form-send")]
-    public class FormSendButtonTagHelper : TagHelper
+    public class FormSend2ButtonTagHelper: TagHelper
     {
-        //  Note:   This tag hlper will only work on "button" tags
-        //              It will also only work in forms, and this button acts as form submission.
-        public override void Process(TagHelperContext context, TagHelperOutput output)
+        private FormButtonTagHelper formButton = new FormButtonTagHelper()
         {
-            //  This looks janky. There's probably a better way of doing it
-            //  Mixing buttons with form-group input is somewhat tricky, and this is how I got it to work
-            output.SetRawPreContentElement(
+            ButtonIcon = "paper-plane",
+            ButtonText = "Send",
+            HTMLTag = "button",
+            HTMLClassList = "btn btn-outline-primary pb-0 mt-3 float-right",
+            Attributes = new() {
+                {"type", "submit"},
+            },
+            RawPreContentElement =
                 "<table class=\"form-group mb-2\">\n" +
                     "\t<tr>\n" +
-                        "\t\t<td>\n"
-            );
-            output.Content.Clear();
-            output.Content.Append("\tSend");
-            output.BuildTag("button", "btn btn-primary pb-0");
-            output.Attributes.SetAttribute("type", "submit");
-            output.SetRawPostContentElement(
+                        "\t\t<td>\n",
+            // I neeed to find a way to separate the icon from the post content elements...
+            RawPostContentElement =
                             "\t\t\t<i class=\"fa fa-paper-plane fa-xl\"></i> \n" +
                         "\t\t</td>\n" +
                     "\t</tr>" +
                 "</table>"
-            );
+        };
+
+        /// <summary>This uses the 'formButton' above to build a form button tag helper.</summary>
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.SetRawPreContentElement(formButton.RawPreContentElement);
+            output.Content.Clear();
+            output.Content.Append(formButton.ButtonText);
+            output.BuildTag(formButton.HTMLTag, formButton.HTMLClassList);
+            foreach (System.Collections.Generic.KeyValuePair<string, string> attribute in formButton.Attributes)
+                output.Attributes.SetAttribute(attribute.Key, attribute.Value);
+            output.SetRawPostContentElement(formButton.RawPostContentElement);
         }
     }
 }
